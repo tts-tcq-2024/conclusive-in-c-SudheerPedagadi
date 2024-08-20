04e01b67bafa66de2c4d2d69c52cc669c13042dd
SudheerPedagadi <pedagadivenkat083@gmail.com>
2024-08-20 20:16:16 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,27 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+TEST(TypeWiseAlertTestSuite, InfersBreachAccordingToLimits) {
+    EXPECT_EQ(inferBreach(12, 20, 30), TOO_LOW);
+    EXPECT_EQ(inferBreach(25, 20, 30), NORMAL);
+    EXPECT_EQ(inferBreach(35, 20, 30), TOO_HIGH);
+}
 
+TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreachCorrectly) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 36), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 39), NORMAL);
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 20), NORMAL);
+}
 
+TEST(TypeWiseAlertTestSuite, SendsAlertToController) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "BrandX"};
+    checkAndAlert(TO_CONTROLLER, batteryChar, 36);
+    // Check stdout or other means to ensure the alert was sent to the controller
+}
+
+TEST(TypeWiseAlertTestSuite, SendsAlertToEmail) {
+    BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "BrandY"};
+    checkAndAlert(TO_EMAIL, batteryChar, 46);
+    // Check stdout or other means to ensure the alert was sent via email
 }
